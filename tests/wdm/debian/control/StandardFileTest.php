dab5976eb3b1df20ad39911a40d58f1800525d3c
Walter Dal Mut <walter.dalmut@gmail.com>
2014-12-03 22:38:07 +0100

    Added standard control file test case

---
+++ tests/wdm/debian/control/StandardFileTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace wdm\debian\control;
+
+class StandardFileTest extends \PHPUnit_Framework_TestCase
+{
+    private $object;
+
+    public function setUp()
+    {
+        $this->object = new StandardFile();
+    }
+
+    public function testMinimumFile()
+    {
+        $conf = (string)$this->object;
+
+        $expected = <<<OEF
+Version: 0.1
+Section: web
+Priority: optional
+Architecture: all
+Essential: no
+Installed-Size: 1024
+Maintainer: name [email]
+Provides: your-company
+Description: Your description
+
+OEF;
+        $this->assertEquals($expected, $conf);
+    }
+}
